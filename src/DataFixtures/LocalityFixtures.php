00e7d75ddaededd2a3934016b7009ed2d41486ff
marie <marie.sagehomme@gmail.com>
2020-05-09 06:54:09 +0200

    Modèle Location +Relation ManyToOne with Locality

--- src/DataFixtures/LocalityFixtures.php
+++ src/DataFixtures/LocalityFixtures.php
@@ -3,36 +3,31 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Locality;
 
 class LocalityFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
-{
+    {
         $localities = [
-            [
-            'postal_code'=>'1000',
-            'locality'=>'Bruxelles'
-            ],
-            [
-            'postal_code'=>'1030',
-            'locality'=>'Scharbeek'
-            ],
-            [
-            'postal_code'=>'1170',
-            'locality'=>'Watermael-Boitsfort'
-            ],
+            ['postal_code'=>'1000','locality'=>'Bruxelles'],
+            ['postal_code'=>'1020','locality'=>'Laeken'],
+            ['postal_code'=>'1030','locality'=>'Schaerbeek'],
+            ['postal_code'=>'1050','locality'=>'Ixelles'],
+            ['postal_code'=>'1070','locality'=>'Anderlecht'],
+            ['postal_code'=>'1180','locality'=>'Uccle'],
         ];
         
-        foreach ($localities as $data) {
-            $localities = new Locality();
-            $localities->setPostalCode($data['postal_code']);
-            $localities->setLocality($data['locality']);
+        foreach ($localities as $record) {
+            $locality = new Locality();
+            $locality->setPostalCode($record['postal_code']);
+            $locality->setLocality($record['locality']);
+            $manager->persist($locality);
             
-            $manager->persist($localities);
+            $this->addReference($record['locality'], $locality);
         }
-
+        
         $manager->flush();
     }
 }
--- src/Entity/Locality.php
+++ src/Entity/Locality.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -27,6 +29,17 @@ class Locality
      */
     private $locality;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Location", mappedBy="locality")
+     */
+    private $locations;
+
+    public function __construct()
+    {
+        $this->locations = new ArrayCollection();
+    }
+    
+    
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +65,36 @@ class Locality
     public function setLocality(string $locality): self
     {
         $this->locality = $locality;
+
+        return $this;
+    }
+/**
+     * @return Collection|Location[]
+     */
+    public function getLocations(): Collection
+    {
+        return $this->locations;
+    }
+
+    public function addLocation(Location $location): self
+    {
+        if (!$this->locations->contains($location)) {
+            $this->locations[] = $location;
+            $location->setLocality($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLocation(Location $location): self
+    {
+        if ($this->locations->contains($location)) {
+            $this->locations->removeElement($location);
+            // set the owning side to null (unless already changed)
+            if ($location->getLocality() === $this) {
+                $location->setLocality(null);
+            }
+        }
 
         return $this;
     }
