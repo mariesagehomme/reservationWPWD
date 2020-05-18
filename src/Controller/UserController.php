8d30075d6c86b33feed1e4656827e90673fa5482
marie <marie.sagehomme@gmail.com>
2020-05-18 12:34:26 +0200

    Edit user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -67,4 +67,24 @@ class UserController extends AbstractController
             'user'=>$user,
         ]);
     }
+    
+    /**
+     * @Route("/user/{id}/edit", name="user_edit", methods={"GET","POST"})
+     */
+     public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('user_index');
+        } 
+        
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+        
+    }
 }
